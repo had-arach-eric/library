c950dc8459d28543a8db1da0b0403e1f90ec8d89
Eric Had Arach <hadarachericj@gmail.com>
2023-08-15 09:45:16 -0300

    Solution of id.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-const myLibrary = [];
+const myLibrary = {};
 const containerUI = document.querySelector(".container");
 const addButtonUI = document.querySelector(".add-button");
 const cardsUI = document.querySelector(".cards");
-let identifier = {val: "0"};
+let id = {val: "0"};
 
-function Book(title, author, numberOfPages, identifier) {
+function Book(title, author, numberOfPages, read, id) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.read = read;
-  this.identifier = identifier.val;
-  toString(Number(identifier.val) + 1);
+  this.id = id.val;
+  id.val = (+(id.val) + 1).toString();
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book);
+  myLibrary[book.id] = book;
 }
 
 function addBookToPage(book) {
@@ -25,6 +25,7 @@ function addBookToPage(book) {
   
   const titleUI = document.createElement("h2");
   titleUI.textContent = `Title: ${book.title}`;
+  cardUI.setAttribute("id", book.id);
   cardUI.appendChild(titleUI);
 
   const authorUI = document.createElement("p");
@@ -50,7 +51,8 @@ function addBookToPage(book) {
   const removeButtonUI = document.createElement("button");
   removeButtonUI.textContent = "Delete";
   removeButtonUI.setAttribute("type", "button");
-  removeButtonUI.setAttribute("data-identifier", identifier.val);
+  removeButtonUI.setAttribute("id", id.val);
+  cardUI.appendChild(removeButtonUI);
 }
 
 function addBook(book) {
@@ -58,21 +60,29 @@ function addBook(book) {
   addBookToPage(book);
 }
 
-function removeBook(identifier) {
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (myLibrary[i].identifier === identifier) {
-      cardsUI.children[]
-      myLibrary.splice(i, 1);
-
+function removeBook(book, id) {
+  for (let key in book) {
+    if (key === identifier) {
+      delete book[key];
     }
   }
+  /*for (let i = 0; i < cardsUI.length; i++) {
+    if (cardsUI.children[i] === )
+  }*/
 }
 
 
 
-const libro1 = new Book("Harry Potter", "JK", 500, false);
-const libro2 = new Book("Batman", "Nolan", "250", true);
+const libro1 = new Book("Harry Potter", "JK", 500, false, id);
+const libro2 = new Book("Batman", "Nolan", "250", true, id);
+//console.log(libro1);
+//console.log(libro2);
 
 addBook(libro1);
 addBook(libro2);
+
+console.log(myLibrary);
+console.log(cardsUI);
+
+
 
