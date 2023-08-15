d7653c6a5c7f429ce55501ac671d5eb3b2ad5954
Eric Had Arach <hadarachericj@gmail.com>
2023-08-15 09:03:17 -0300

    With array.

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,78 @@
-/*
-Todos tus objetos libro van a ser almacenados en un simple array, así que añade una función al script (no el constructor) que pueda tomar la entrada del usuario y almacenar los nuevos objetos libro en un array. Tu código debería ser algo como esto:
-*/
-
 const myLibrary = [];
+const containerUI = document.querySelector(".container");
+const addButtonUI = document.querySelector(".add-button");
+const cardsUI = document.querySelector(".cards");
+let identifier = {val: "0"};
 
-function Book(title, author, numberOfPages, read) {
+function Book(title, author, numberOfPages, identifier) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.read = read;
+  this.identifier = identifier.val;
+  toString(Number(identifier.val) + 1);
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-/*
-Escribe una función que recorra el array y muestre cada libro en la página. Puedes mostrarlos en algún tipo de tabla, o cada uno en su propia "tarjeta". Por ahora puede ser útil añadir manualmente algunos libros a tu matriz para que puedas ver la pantalla.
-*/
+function addBookToPage(book) {
+  
+  const cardUI = document.createElement("div");
+  cardUI.classList.add("card");
+  cardsUI.appendChild(cardUI);
+  
+  const titleUI = document.createElement("h2");
+  titleUI.textContent = `Title: ${book.title}`;
+  cardUI.appendChild(titleUI);
+
+  const authorUI = document.createElement("p");
+  authorUI.textContent = `Author: ${book.author}`;
+  cardUI.appendChild(authorUI);
+
+  const numberOfPagesUI = document.createElement("p");
+  numberOfPagesUI.textContent = `Number of pages: ${book.numberOfPages}`;
+  cardUI.appendChild(numberOfPagesUI);
+
+  const readUI = document.createElement("p");
+  const labelReadUI = document.createElement("label");
+  labelReadUI.textContent = "Read";
+  const inputReadUI = document.createElement("input");
+  inputReadUI.setAttribute("type", "checkbox");
+  inputReadUI.checked = (book.read) ? true : false;
+  readUI.appendChild(labelReadUI); 
+  readUI.appendChild(inputReadUI);
+  cardUI.appendChild(readUI);
+  readUI.style.display = "flex";
+  readUI.style.columnGap = "10px";
+
+  const removeButtonUI = document.createElement("button");
+  removeButtonUI.textContent = "Delete";
+  removeButtonUI.setAttribute("type", "button");
+  removeButtonUI.setAttribute("data-identifier", identifier.val);
+}
+
+function addBook(book) {
+  addBookToLibrary(book);
+  addBookToPage(book);
+}
+
+function removeBook(identifier) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].identifier === identifier) {
+      cardsUI.children[]
+      myLibrary.splice(i, 1);
+
+    }
+  }
+}
 
 
 
+const libro1 = new Book("Harry Potter", "JK", 500, false);
+const libro2 = new Book("Batman", "Nolan", "250", true);
 
-/*
-Añade un botón "NUEVO LIBRO" que muestre un formulario que permita a los usuarios introducir los datos del nuevo libro: autor, título, número de páginas, número de páginas leídas, si se ha leído y cualquier otra cosa que desees. Usted decide cómo mostrar este formulario. Por ejemplo, puede que desee mostrar un formulario en una barra lateral o puede que desee explorar diálogos y modales utilizando la etiqueta <dialog>. 
-No importa cómo lo hagas, lo más probable es que te encuentres con un problema en el que enviar el formulario no hará lo que esperas que haga. Eso es porque el input submit intenta enviar los datos a un servidor por defecto. Si has hecho la sección extra para la tarea de la calculadora, puede que estés familiarizado con event.preventDefault();. Vuelve a leer la documentación de event.preventDefault y verás cómo puedes resolver este problema.
-*/
+addBook(libro1);
+addBook(libro2);
 
-/*
-Añade un botón en la pantalla de cada libro para eliminarlo de la biblioteca. Necesitarás asociar tus elementos DOM con los objetos libro de alguna manera. Una solución fácil es darles un data-attribute que corresponde al índice de la matriz de la biblioteca.
-*/
-
-/*
-Añade un botón en la pantalla de cada libro para cambiar su estado de lectura. Para facilitar esto, querrás crear la función que cambia el estado de lectura de un libro en tu prototipo de Libro.
-*/
